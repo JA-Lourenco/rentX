d3f185196dcf34ebbd0a449b2481aac4be12bbd4
JA-Lourenco <ja.lourenco@outlook.com>
2022-05-10 13:31:13 -0300

    Creating the Scheduling screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 // import { Home } from './src/screens/Home';
-import { CarDetails } from './src/screens/CarDetails';
+// import { CarDetails } from './src/screens/CarDetails';
+import { Scheduling } from './src/screens/Scheduling';
 
 import { ThemeProvider } from 'styled-components';
 
@@ -36,7 +37,8 @@ export default function App() {
   return (
     <ThemeProvider theme={ theme }>
       {/* <Home /> */}
-      <CarDetails />
+      {/* <CarDetails /> */}
+      <Scheduling />
     </ThemeProvider>
   );
 }
---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import { BackButton } from '../../components/BackButton';
+import { Button } from '../../components/Button';
+
+import { useTheme } from 'styled-components';
+import { StatusBar } from 'react-native';
+
+import ArrowSvg from '../../assets/arrow.svg'
+
+import {
+    Container,
+    Header,
+    Title,
+    RentalPeriod,
+    DateInfo,
+    DateTitle,
+    DateValue,
+    Content,
+    Footer
+} from './styles';
+
+export function Scheduling(){
+    const theme = useTheme()
+
+    return (
+        <Container>
+            <StatusBar 
+                barStyle='light-content'
+                backgroundColor='transparent'
+                translucent
+            />
+            
+            <Header>
+                <BackButton 
+                    onPress={() => {}}
+                    color={theme.colors.background_secondary}
+                />
+
+                <Title>
+                    Escolha uma{'\n'}data de início e{'\n'}fim do aluguel
+                </Title>
+
+                <RentalPeriod>
+                    <DateInfo>
+                        <DateTitle>DE</DateTitle>
+
+                        <DateValue selected={false}>
+                            18/06/2021
+                        </DateValue>
+                    </DateInfo>
+
+                    <ArrowSvg />
+
+                    <DateInfo>
+                        <DateTitle>ATÉ</DateTitle>
+
+                        <DateValue selected={false}>
+                            18/06/2021
+                        </DateValue>
+                    </DateInfo>
+                </RentalPeriod>
+            </Header>
+
+            <Content>
+
+            </Content>
+
+            <Footer>
+                <Button 
+                    title='Confirmar'
+                />
+            </Footer>
+            
+        </Container>
+    )
+}
