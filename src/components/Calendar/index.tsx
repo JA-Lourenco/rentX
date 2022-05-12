76e149d014b00cc41247e81c71b7b85c89dd0e29
JA-Lourenco <ja.lourenco@outlook.com>
2022-05-12 14:11:03 -0300

    Refactoring the locale config of the calendar

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -2,19 +2,14 @@ import React from 'react';
 
 import Feather from 'react-native-vector-icons/Feather'
 import { useTheme } from 'styled-components';
+import ptBR from './localeConfig';
 
 import { 
     Calendar as CustomCalendar,
     LocaleConfig
 } from 'react-native-calendars'
 
-LocaleConfig.locales['pt-br'] = {
-    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Nov', 'Dez'],
-    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-    dayNamesShort: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB'],
-    today: 'Hoje'
-}
+LocaleConfig.locales['pt-br'] = ptBR
 LocaleConfig.defaultLocale = 'pt-br'
 
 export function Calendar(){
