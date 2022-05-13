a56eace45ffa4240e4648853b8ef7883d48bc7b8
JA-Lourenco <ja.lourenco@outlook.com>
2022-05-13 11:30:54 -0300

    GET method: getting my rented cars

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { BackButton } from '../../components/BackButton';
+
 import { StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+import { useNavigation } from '@react-navigation/native';
+import { CarDTO } from '../../dtos/CarDTO';
 
 import {
-    Container
+    Container,
+    Header,
+    Title,
+    Paragraph
 } from './styles';
+import api from '../../services/api';
 
 export function MyCars(){
+    const [cars, setCars] = useState<CarDTO[]>([])
+    const [loading, setLoading] = useState(true)
+
+    const theme = useTheme()
+
+    const navigation = useNavigation()
+
+    function handleGoBack() {
+        navigation.goBack()
+    }
+
+    useEffect(() => {
+        async function fetchMyCars() {
+            try {
+                const response = await api.get('/schedules_byuser?user_id=1')
+                setCars(response.data)
+            } catch (error) {
+                console.log('Screen: MyCars\nfunction:fetchMyCars\nerror', error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchMyCars()
+    }, [])
+
     return (
         <Container>
             <StatusBar 
@@ -13,6 +49,21 @@ export function MyCars(){
                 backgroundColor='transparent'
                 translucent
             />
+
+            <Header>
+                <BackButton
+                    color={theme.colors.background_secondary}
+                    onPress={handleGoBack}
+                />
+
+                <Title>
+                    Seus agendamentos,{'\n'}estão aqui.
+                </Title>
+
+                <Paragraph>
+                    Conforto,segurança e praticidade.
+                </Paragraph>
+            </Header>
 
         </Container>
     )
--- src/screens/MyCars/styles.ts
+++ src/screens/MyCars/styles.ts
@@ -1,5 +1,29 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
     flex: 1;
 `;
+
+export const Header = styled.View`
+    width: 100%;
+    height: 273px;
+    background-color: ${({ theme }) => theme.colors.header};
+    justify-content: center;
+    padding: ${getStatusBarHeight() + 30}px 25px 25px;
+`
+
+export const Title = styled.Text`
+    font-size: ${RFValue(30)}px;
+    font-family: ${({ theme }) => theme.fonts.secondary_600};
+    color: ${({ theme }) => theme.colors.background_secondary};
+    margin-top: 22px;
+`
+
+export const Paragraph = styled.Text`
+    font-size: ${RFValue(15)}px;
+    font-family: ${({ theme }) => theme.fonts.secondary_400};
+    color: ${({ theme }) => theme.colors.background_secondary};
+    margin-top: 18px;
+`
