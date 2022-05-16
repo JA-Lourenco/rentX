3d38613e9e97c554efdf4014a629fea54c6c25e4
JA-Lourenco <ja.lourenco@outlook.com>
2022-05-16 13:33:30 -0300

    Finishing the car image slider

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
-import { FlatList } from 'react-native';
+import { FlatList, ViewToken } from 'react-native';
 
 import {
     Container,
@@ -14,7 +14,19 @@ interface ImageSliderProps {
     imagesUrl: string[]
 }
 
+interface ChangeImageProps {
+        viewableItems: ViewToken[];
+        changed: ViewToken[];    
+}
+
 export function ImageSlider({ imagesUrl }: ImageSliderProps){
+    const [imageIndex, setImageIndex] = useState(0)
+
+    const indexChanged = useRef((info: ChangeImageProps) => {
+        const index = info.viewableItems[0].index!
+        setImageIndex(index)
+    })
+
     return (
         <Container>
             <ImageIndexes>
@@ -22,7 +34,7 @@ export function ImageSlider({ imagesUrl }: ImageSliderProps){
                     imagesUrl.map((_, index) => 
                         <ImageIndex 
                             key={String(index)}
-                            active={true} 
+                            active={index === imageIndex} 
                         />
                     )
                 }
@@ -41,6 +53,7 @@ export function ImageSlider({ imagesUrl }: ImageSliderProps){
                 }
                 horizontal
                 showsHorizontalScrollIndicator={false}
+                onViewableItemsChanged={indexChanged.current}
             />              
         </Container>
     )
