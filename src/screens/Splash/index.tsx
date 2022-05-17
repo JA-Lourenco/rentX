e92fe417bb75cfc67166c276ee89ab3c62e87e3d
JA-Lourenco <ja.lourenco@outlook.com>
2022-05-17 10:19:24 -0300

    Splash screen animations

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,4 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import BrandSvg from '../../assets/brand.svg'
+import LogoSvg from '../../assets/logo.svg'
+
+import Animated, { 
+    useSharedValue, 
+    useAnimatedStyle,
+    withTiming,
+    interpolate,
+    Extrapolate
+} from 'react-native-reanimated';
 
 import { StatusBar } from 'react-native';
 
@@ -7,6 +18,45 @@ import {
 } from './styles';
 
 export function Splash(){
+    const logoAnimation = useSharedValue(0)
+
+    const brandStyle = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(logoAnimation.value, [0, 50], [1, 0],),
+            transform: [
+                {
+                    translateX: interpolate(logoAnimation.value,
+                        [0, 50],
+                        [0, -50],
+                        Extrapolate.CLAMP
+                    )
+                }
+            ]
+        }
+    })
+
+    const logoStyle = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(logoAnimation.value, [0, 25, 50], [0, .3, 1]),
+            transform: [
+                {
+                    translateX: interpolate(logoAnimation.value,
+                        [0, 50],
+                        [-50, 0],
+                        Extrapolate.CLAMP
+                    )
+                }
+            ]
+        }
+    })
+
+    useEffect(() => {
+        logoAnimation.value = withTiming(
+            50,
+            {duration: 2000}
+        )
+    }, [])
+
     return (
         <Container>
             <StatusBar 
@@ -14,6 +64,14 @@ export function Splash(){
                 backgroundColor='transparent'
                 translucent
             />
+
+            <Animated.View style={[brandStyle, {position: 'absolute'}]}>
+                <BrandSvg width={80} height={50} />
+            </Animated.View>
+
+            <Animated.View style={[logoStyle, {position: 'absolute'}]}>
+                <LogoSvg width={180} height={20} />
+            </Animated.View>
         </Container>
     )
 }
